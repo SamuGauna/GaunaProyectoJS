b743603032d87d3023b2cba52ed15c77a08aa7e0
samu.el <samuelgauna78@gmail.com>
2022-11-22 23:30:25 -0300

    tercera pre entrega

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 
-let respuesta;
-let encuesta;
-let cantidadCD;
 
 let carrito = [];
 
@@ -12,112 +9,109 @@ const discoFiltro = [
     { nombre: "Slim Shady", precio: 250},
     { nombre: "MMLP", precio: 300},
 ];
+const discosArreglo = [
+  {
+    id: "disco1",
+    nombre: "eminem show",
+    precio: 100
+  },
+  {
+    id: "disco2",
+    nombre: "kamikaze",
+    precio: 150
+  },
+  {
+    id: "disco3",
+    nombre: "mmlp",
+    precio: 200
+  },
+  {
+    id: "disco4",
+    nombre: "slim shady",
+    precio: 250
+  },
+  {
+    id: "disco5",
+    nombre: "mmlp 2",
+    precio: 300
+  },
+  {
+    id: "disco6",
+    nombre: "relapse",
+    precio: 350
+  },
+];
 
-//constructor, Objetos a partir de una funcion
 
-function Discos(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-}
-const discoK = new Discos("Kamikaze", 100);
-const discoR = new Discos("Relapse", 150);
-const discoReco = new Discos("Recovery", 200);
-const discoS = new Discos("Slim Shady", 250);
-const discoM = new Discos("MMLP", 300);
+let botonesAgregar = document.querySelectorAll(".disco-agregar");
 
-//funcion total de compra
-
-function compra(cd) {
-    totalCompra = cantidadCD * cd;
-    alert(nombreUsuario + " el total de tu compra es de " + totalCompra);
+function actualizandoBotones() {
+  botonesAgregar.forEach(boton => {
+    boton.addEventListener("click", agregarAlCarrito);
+  });
+}
+actualizandoBotones();
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const discoAgregado = discosArreglo.find(disco => disco.id === idBoton);
+  
+  if(carrito.some(disco => disco.id === idBoton)) {
+    const index = carrito.findIndex(disco => disco.id === idBoton);
+    carrito[index].cantidad++;
+  }
+  else {
+    discoAgregado.cantidad = 1;
+    carrito.push(discoAgregado);
+  }
+  resumenCompra();
+  function resumenCompra(){
+  
+    const tabla = document.getElementById('tabla')
+    const creacionTabla = document.createElement('table')
+    creacionTabla.innerHTML = `<table class="table ">
+    <thead>
+      <tr>
+        <th scope="col"> Cantidad </th>
+        <th scope="col"> Nombre del Disco </th>
+        <th scope="col"> Precio </th>       
+        <th scope="col"> Total </th>       
+      </tr>
+    </thead>
+    <tbody id="creandoFila" class="table-group-divider">
+      <tr>
+        <th scope="row">${discoAgregado.cantidad}</th>
+        <td>${discoAgregado.nombre}</td>
+        <td>${discoAgregado.precio}</td>
+        <td>total</td>
+      </tr>      
+    </tbody>
+    </table>`
+    tabla.appendChild(creacionTabla)
+  
+  
+    const divBotones = document.getElementById('botones')
+    const crearBotones = document.createElement('div')
+    crearBotones.innerHTML = `<button id="confirmarCompra">Confirmar Compra</button>`
+    divBotones.appendChild(crearBotones)
+  
+    const confirmandoCompra = document.getElementById("confirmarCompra");
+    console.log(carrito);
+  }
+  localStorage.setItem("discos-en-carrito", JSON.stringify(carrito));
 }
 
-//Codigo
 
-let nombreUsuario = prompt("Hola! Por favor ingresa tu nombre");
-let respFiltro = prompt(nombreUsuario + " deseas buscar algun disco en particular?Y/N");
-if (respFiltro === "Y" || respFiltro === "y") {
-    let discoBuscado = prompt("Coloca el nombre del disco que deseas buscar, no te olvides revisar la consola");
-    console.log( discoFiltro.find( (discoFiltro) => discoFiltro.nombre == discoBuscado ) ); 
+function creandoFilas() {    
+  let fila1 = document.createElement('tr')
+  fila1.innerHTML = `<td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>`
+  let fila = document.getElementById("creandoFila");
+  idBoton.onclick = () => {
+  fila.appendChild(fila1)
+  }
 }
-alert ("Hola" + " " + nombreUsuario + " " + "a continuacion te vamos a mostrar los discos que tenemos disponibles, para las opciones no te olvides que es colocando el numero")
-let discoOpcion = parseInt(prompt("Que disco deseas comprar? \n 1.Relapse \n 2.Recovery \n 3.Slim Shady LP \n 4.MMMLP \n 5.Kamikaze"));
 
-if (discoOpcion === 1) {
-    alert( nombreUsuario + " el disco relapse cuesta " + discoR.precio);
-    cantidadCD = parseInt(prompt("Cuantos cds necesitas comprar " + nombreUsuario + "?"));
-    compra(discoR.precio);
-    respuesta = prompt("Confirmar la compra?Y/N");
-    if (respuesta === "Y" || respuesta === "y") {
-        alert("Agregado al carrito, tu carrito se vera reflejado en console :)");
-        carrito.push(discoR.nombre, cantidadCD);
-        console.log(carrito);
-    }
-    else {
-        alert("Ok :(");
-    }
-}
-else if (discoOpcion === 2) {
-    alert( nombreUsuario + " " + "el disco recovery cuesta" + " " + discoReco.precio);
-    cantidadCD = parseInt(prompt("Cuantos cds necesitas comprar " + nombreUsuario));
-    compra(discoReco.precio);
-    respuesta = prompt("Confirmar la compra?Y/N");
-    if (respuesta == "Y" || respuesta == "y") {
-        alert("Agregado al carrito, tu carrito se vera reflejado en console :)");
-        carrito.push(discoReco.nombre, cantidadCD);
-        console.log(carrito);
-    }
-    else {
-        alert("Ok :(");
-    }
-}
-else if (discoOpcion === 3) {
-    alert( nombreUsuario + " " + "el disco Slim Shady LP cuesta" + " " + discoS.precio);
-    cantidadCD = parseInt(prompt("Cuantos cds necesitas comprar " + nombreUsuario));
-    compra(discoS.precio);
-    respuesta = prompt("Confirmar la compra?Y/N");
-    if (respuesta === "Y" || respuesta === "y") {
-        alert("Agregado al carrito, tu carrito se vera reflejado en console :)");
-        carrito.push(discoS.nombre, cantidadCD);
-        console.log(carrito);
-    }
-    else {
-        alert("Ok :(");
-    }
-}
-else if (discoOpcion === 4) {
-    alert( nombreUsuario + " " + "el disco MMLP cuesta" + " " + discoM.precio);
-    cantidadCD = prompt("Cuantos cds necesitas comprar " + nombreUsuario);
-    compra(discoM.precio);
-    respuesta = prompt("Confirmar la compra?Y/N");
-    if (respuesta === "Y" || respuesta === "y") {
-        alert("Agregado al carrito, tu carrito se vera reflejado en console :)");
-        carrito.push(discoM.nombre, cantidadCD);
-        console.log(carrito);
-    }
-    else {
-        alert("Ok :(");
-    }
-}
-else if (discoOpcion === 5) {
-    alert( nombreUsuario + " " + "el disco kamikaze cuesta" + " " + discoK.precio);
-    cantidadCD = parseInt(prompt("Cuantos cds necesitas comprar " + nombreUsuario));
-    compra(discoK.precio);
-    respuesta = prompt("Confirmar la compra?Y/N");
-    if (respuesta === "Y" || respuesta === "y") {
-        alert("Agregado al carrito, tu carrito se vera reflejado en console :)");
-        carrito.push(discoK.nombre, cantidadCD);
-        console.log(carrito);
-    }
-    else {
-        alert("Ok :(");
-    }
-}
-else {
-    alert("No seleccionaste ninguna de las opciones correctas");
-}
-encuesta = prompt("Del 1 al 5, Como calificarias nuestra atencion?");
-for (encuesta; encuesta >=1 && encuesta <=5; encuesta++) {
-    console.log("Gracias por evaluarnos");
-}
+
+
 
